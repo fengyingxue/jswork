5fa5f8faeee3f1b38a362ff681b442d2e8340ddc
fengyingxue <55180606+fengyingxue@users.noreply.github.com>
2019-10-16 20:32:24 +0800

    Add files via upload

---
+++ work6/pai.js
@@ -0,0 +1,24 @@
+function calculate(){
+    let num = document.getElementById('array').value
+    num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById('array').value = pi(num)
+    }
+}
+function factorial(n){
+    let sum = 1
+    for(let i=1;i<=n;++i){
+        sum += arr[i]
+    }
+    return sum 
+}
+function oddFactorial(n){
+    let sum = 0
+    for(let i=0;i<num;++i){
+        let dividend = factorial()
+        let divisor = oddFactorial()
+         sum * n(sum - 1);
+    }
+    return sum /2;
+}
+calculate()
